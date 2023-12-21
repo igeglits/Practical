6f12f9e443122cf8f6d1b9c453584079cd581cfb
Igor Eglit <igeglits@gmail.com>
2023-12-21 22:45:54 +0200

    factorial recursion

---
+++ src/Factorial/FactorialRecursion.java
@@ -0,0 +1,17 @@
+package Factorial;
+
+
+
+class FactorialRecursion {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    private static int factorial(int i) {
+        if (i==0){
+          return 1;
+        }else{
+         return i*factorial(i-1);
+        }
+    }
+}
