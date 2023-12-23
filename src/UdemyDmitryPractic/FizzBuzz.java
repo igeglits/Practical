cf5e0498072a4d2019f4f647bf1400df3afdd8de
Igor Eglit <igeglits@gmail.com>
2023-12-23 21:10:52 +0200

    FizzBuzz

---
+++ src/UdemyDmitryPractic/FizzBuzz.java
@@ -0,0 +1,22 @@
+package UdemyDmitryPractic;
+
+class FizzBuzz {
+    public static void main(String[] args) {
+        cycle();
+    }
+
+    private static void cycle() {
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
