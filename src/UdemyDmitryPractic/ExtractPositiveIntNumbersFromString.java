eb911b37af52aabd640c4805a6f7981ca1c6249b
igor eglit <igeglits@gmail.com>
2024-01-26 11:24:55 +0200

    выделение чисел из строки

--- src/UdemyDmitryPractic/ExtractPositiveIntNumbersFromString.java
+++ src/UdemyDmitryPractic/ExtractPositiveIntNumbersFromString.java
@@ -1,6 +1,7 @@
 package UdemyDmitryPractic;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class ExtractPositiveIntNumbersFromString {
     public static void main(String[] args) {
@@ -13,7 +14,35 @@ class ExtractPositiveIntNumbersFromString {
     }
 
     private static int[] extractNumbersFromString(final String string) {
-        // Your implementation must be here
-        return null;
+        char[] charArray = string.toCharArray();
+        int[] numbers = new int[string.length()];
+        int count = 0;
+        StringBuilder numberBuilder = new StringBuilder();
+        for (int i = 0; i < charArray.length; i++) {
+            var c = charArray[i];
+            if (Character.isDigit(c)) {
+                numberBuilder.append(c);
+            } else if (!numberBuilder.isEmpty()){
+                numbers[count++] = Integer.parseInt(numberBuilder.toString());
+                numberBuilder.setLength(0);
+            }
+        }
+        //зайдя на последний круг с последним символом цикл после проверки не придет на 26 строку, ее надо повторить после цикла для последнего стрингбилдера
+        if (!numberBuilder.isEmpty())
+            numbers[count++] = Integer.parseInt(numberBuilder.toString());
+
+/*return Arrays.stream(numbers)
+        .map(numbers::)
+        .collect(Collectors.toList());*/
+      return trimArray(numbers, count);
+
+    }
+
+    private static int[] trimArray(int[] array, int count) {
+int[] result = new int[count];
+        for (int i = 0; i < count; i++) {
+            result[i]=array[i];
+        }
+        return result;
     }
 }
